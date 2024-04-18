73ae4663ca40286447a6b11806b565f4c175a7eb
Nakum-Okello <okellonahum12@gmail.com>
2024-04-18 11:39:35 +0300

    Added swagger depency

---
+++ bookstore/src/main/java/com/crni99/bookstore/controller/emailService.java
@@ -0,0 +1,10 @@
+package com.crni99.bookstore.controller;
+
+public interface emailService {
+
+    static void sendEmail(String email, String string, String string2) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'sendEmail'");
+    }
+
+}
